a028002e764dafc1a09ce65e916545b434f8273d
talicos <thallycosta82@gmail.com>
2026-05-22 01:11:52 -0300

    Fix: gerando arquivos de migração do app core

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('dashboard/', views.dashboard_view, name='dashboard'),
+    path('', views.dashboard_view, name='dashboard'),
     path('configuracoes/', views.settings_view, name='settings_page'),
 ]
