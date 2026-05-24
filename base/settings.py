268b7d26dbca7c654bbd403a5bc125df303c62d9
talicos <thallycosta82@gmail.com>
2026-05-23 21:59:56 -0300

    Fix: limpando sintaxe do ALLOWED_HOSTS

--- base/settings.py
+++ base/settings.py
@@ -31,10 +31,10 @@ SECRET_KEY = 'django-insecure-5%+5v$$65dq^er(7eev=jwmq!wera_k9j5%gr+-mp2vuco2zzw
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'portalnexus360.onrender.com', 
-    'localhost', 
+    'portalnexus360.onrender.com',
+    'localhost',
     '127.0.0.1',
-    '.onrender.com' # O ponto no início aceita qualquer variação do Render de forma segura
+    '.onrender.com',
 ]
 
 
