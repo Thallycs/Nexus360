c34db5f3c42f8671421d1ccbd35bf8bfb0a95c55
talicos <thallycosta82@gmail.com>
2026-05-23 21:18:08 -0300

    Feature: adicionando tela inicial de login integrada ao design neon

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.dashboard_view, name='dashboard'),
-    path('configuracoes/', views.settings_view, name='settings_page'),
+    path('dashboard/', views.dashboard, name='dashboard'),          # Rota de Projetos
+    path('equipes/', views.equipes, name='equipes_page'),            # Rota de Equipes
+    path('financas/', views.financas, name='financas_page'),          # Rota de Finanças
+    path('relatorios/', views.relatorios, name='relatorios_page'),    # Rota de Relatórios
+    path('configuracoes/', views.configuracoes, name='settings_page'),# Rota de Configurações
 ]
