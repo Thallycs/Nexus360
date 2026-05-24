2bb9114e629f978ace80a16e74376b9ea1dbba46
talicos <thallycosta82@gmail.com>
2026-05-24 15:29:55 -0300

    Fix: removendo templates nulos de e-mail para usar o padrao do django

--- core/urls.py
+++ core/urls.py
@@ -10,13 +10,12 @@ urlpatterns = [
     path('cadastro/', views.cadastro_view, name='cadastro'),
     
     # 3. Fluxo de Recuperação de Senha ("Esqueci minha senha")
+    # REMOVIDO: Arquivos faltantes de e-mail deletados para usar o padrão seguro do Django
     path('esqueci-senha/', auth_views.PasswordResetView.as_view(
-        template_name='core/esqueci_senha.html',
-        email_template_name='core/password_reset_email.html',
-        subject_template_name='core/password_reset_subject.txt'
+        template_name='core/esqueci_senha.html'
     ), name='password_reset'),
     
-    # 4. Confirmação de envio do e-mail (Onde gerou o erro TemplateDoesNotExist)
+    # 4. Confirmação de envio do e-mail
     path('esqueci-senha/sucesso/', auth_views.PasswordResetDoneView.as_view(
         template_name='core/esqueci_senha_enviado.html'
     ), name='password_reset_done'),
