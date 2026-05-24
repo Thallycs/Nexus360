b606c9fc54d5069bdd450e65d1ebe083d1d229e7
talicos <thallycosta82@gmail.com>
2026-05-24 14:11:58 -0300

    Fix: forçando execucao do migrate direto no script de entrada wsgi

--- build.sh
+++ build.sh
@@ -2,11 +2,11 @@
 # exit on error
 set -o errexit
 
-# Instala as dependências
+# Instala as dependências do projeto
 pip install -r requirements.txt
 
-# Coleta os arquivos estáticos
+# Coleta os arquivos estáticos de estilo
 python manage.py collectstatic --noinput
 
-# Executa as migrações do banco de dados
-python manage.py migrate
+# Força a criação das tabelas diretamente no PostgreSQL do Render
+python manage.py migrate --run-syncdb
